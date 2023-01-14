564e59e477df3e77bfc0c473a015ba9044d6d00f
Patryk <patryk.piasecki1@gmail.com>
2023-01-14 17:20:18 +0100

    implemented feature to remove the account

--- 04_Bankist_app/script.js
+++ 04_Bankist_app/script.js
@@ -152,7 +152,22 @@ transferForm.addEventListener('submit', e => {
     }
 
     inputTransferAmount.value = inputTransferTo.value = '';
+});
 
+btnClose.addEventListener('click', e => {
+    e.preventDefault();
+    if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin ) {
+        const index = accounts.findIndex( acc => acc.username === currentAccount.username)
+
+        
+        // Delete account
+        accounts.splice(index, 1)
+
+        // Hide UI
+        containerApp.style.opacity = "0";
+    }
+
+    inputCloseUsername.value = inputClosePin.value = ""
 })
 
 
